712d1857b2f4a0f9da1bfdf03176bfb5eed06451
katty <a20160555@pucp.edu.pe>
2020-07-06 21:06:45 -0500

    katty completo

--- index.js
+++ index.js
@@ -24,3 +24,69 @@ conn.connect(function (err) {
 app.listen(8080, function () {
     console.log("Servidor levantado exitosamente");
 });
+
+
+//localhost:8080/centrospoblados/get
+app.get("/centrospoblados/get", function(request,response){
+    var query = "SELECT * FROM inventariotest.centrospoblados";
+    conn.query(query, function (err, result) {
+        if (err) {
+            console.log(err);
+        } else {
+            response.json(result);
+        }
+    })
+});
+
+//localhost:8080/centrospoblados/get/{id}
+app.get("/centrospoblados/get/:id", function(request,response){
+    var idCP = request.params.id;
+    var query = "SELECT * FROM inventariotest.centrospoblados WHERE idCentroPoblado = " + idCP;
+    conn.query(query, function (err, result) {
+        if (err) {
+            console.log(err);
+        } else {
+            response.json(result);
+        }
+    })
+});
+
+//localhost:8080/centrospoblados/create
+app.post("/centrospoblados/create", function(request,response){
+    var nombreCentroPoblado = request.body.nombreCentroPoblado;
+    var ubigeo = request.body.ubigeo;
+    var query = "INSERT INTO inventariotest.centrospoblados (nombreCentroPoblado, ubigeo) VALUES (?,?)";
+    var parametros = [nombreCentroPoblado, ubigeo];
+    var query2= "SELECT * FROM inventariotest.centrospoblados ORDER BY idCentroPoblado DESC LIMIT 1";
+    conn.query(query, parametros,function (err, result) {
+        if (err) {
+            console.log(err);
+        } else {
+            var jsonRespuesta = {idCentroPoblado: result.insertId,nombreCentroPoblado: nombreCentroPoblado, ubigeo: ubigeo};
+            response.json(jsonRespuesta);
+        }
+    })
+});
+
+//localhost:8080/centrospoblados/update
+app.post("/centrospoblados/update", function(request,response){
+    var idCentroPoblado = request.body.idCentroPoblado;
+    var nombreCentroPoblado = request.body.nombreCentroPoblado;
+    var ubigeo = request.body.ubigeo;
+    var query = "UPDATE inventariotest.centrospoblados SET nombreCentroPoblado = ?, ubigeo = ? WHERE idCentroPoblado = ?";
+    var parametros = [nombreCentroPoblado, ubigeo,idCentroPoblado];
+    var query2 = "SELECT * FROM inventariotest.centrospoblados WHERE idCentroPoblado = " + idCentroPoblado;
+    conn.query(query,parametros, function (err, result) {
+        if (err) {
+            console.log(err);
+        } else {
+            conn.query(query2,function (err, result) {
+                if (err) {
+                    console.log(err);
+                } else {
+                    response.json(result);
+                }
+            })
+        }
+    })
+});
